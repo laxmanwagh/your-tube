9f244cb4c1a4b1a0718c1b96547f0bb68b3a54af
laxmanwagh <waghl0334@gmail.com>
2025-06-01 15:38:25 +0530

    Update VideoCall.js

--- client/src/Components/VideoCall/VideoCall.js
+++ client/src/Components/VideoCall/VideoCall.js
@@ -37,8 +37,8 @@ const VideoCall = () => {
     });
 
   useEffect(() => {
-    //socket.current = io.connect("https://youtubeclone-nullclass.onrender.com/");
-    socket.current = io.connect("http://localhost:5000/");
+    socket.current = io.connect("https://youtubeclone-nullclass.onrender.com/");
+    //socket.current = io.connect("http://localhost:5000/");
     console.log("Use Effect running");
 
     navigator.mediaDevices
